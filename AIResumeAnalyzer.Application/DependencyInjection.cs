a442986c0afabe80b41c1192b47f03b2daa362ed
Karthik4111 <karthik.dasari6676@gmail.com>
2026-07-31 20:06:24 +0530

    Add project files.

---
+++ AIResumeAnalyzer.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AIResumeAnalyzer.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
